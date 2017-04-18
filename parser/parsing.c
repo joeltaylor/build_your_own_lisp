73f0da493636269b49a2a41326bced2e0c9dd518
Joel Taylor <joel@codeprivy.com>
2017-04-18 09:44:20 -0700

    Add error handling

--- parser/parsing.c
+++ parser/parsing.c
@@ -62,28 +62,86 @@ int number_of_branches(mpc_ast_t* t){
   return 0;
 }
 
-long eval_op(long x, char* op, long y) {
-  if (strcmp(op, "+") == 0) { return x + y; }
-  if (strcmp(op, "-") == 0) { return x - y; }
-  if (strcmp(op, "*") == 0) { return x * y; }
-  if (strcmp(op, "/") == 0) { return x / y; }
-  if (strcmp(op, "^") == 0) { return pow(x, y); }
-  if (strcmp(op, "%") == 0) { return x % y; }
-  return 0;
+typedef struct {
+  int type;
+  long num;
+  int err;
+} lval;
+
+/* Create Enumberation of possible lval types */
+enum { LVAL_NUM, LVAL_ERR };
+
+/* Create Enumeration of possible error types */
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
+/* Create a new number type lval */
+lval lval_num(long x){
+  lval v;
+  v.type = LVAL_NUM;
+  v.num = x;
+  return v;
 }
 
-long eval(mpc_ast_t* t) {
+lval lval_err(int x){
+  lval v;
+  v.type = LVAL_ERR;
+  v.err = x;
+  return v;
+}
+
+void lval_print(lval v) {
+  switch (v.type) {
+    case LVAL_NUM: printf("%li", v.num); break;
+    case LVAL_ERR: 
+      if (v.err == LERR_DIV_ZERO) {
+        printf("Error: Division by zero!");
+      }
+      if (v.err == LERR_BAD_OP) {
+        printf("Error: Invalid operator!");
+      }
+      if (v.err == LERR_BAD_NUM) {
+        printf("Error: Invalid number!");
+      }
+      break;
+  }
+}
+
+void lval_println(lval v) { lval_print(v); putchar('\n'); }
+
+lval eval_op(lval x, char* op, lval y) {
+
+  /* If either value is an error return it */
+  if (x.type == LVAL_ERR) { return x; }
+  if (y.type == LVAL_ERR) { return y; }
+
+  if (strcmp(op, "+") == 0) { return lval_num(x.num + y.num); }
+  if (strcmp(op, "-") == 0) { return lval_num(x.num - y.num); }
+  if (strcmp(op, "*") == 0) { return lval_num(x.num * y.num); }
+  if (strcmp(op, "^") == 0) { return lval_num(pow(x.num, y.num)); }
+  if (strcmp(op, "%") == 0) { return lval_num(x.num % y.num); }
+  if (strcmp(op, "/") == 0) {
+    /* If second operand is zero return error */
+    return y.num == 0 ? lval_err(LERR_DIV_ZERO) : lval_num(x.num / y.num);
+  }
+
+  return lval_err(LERR_BAD_OP);
+}
+
+lval eval(mpc_ast_t* t) {
 
   /* If tagged as a number return it directly */
   if(strstr(t->tag, "number")) {
-    return atoi(t->contents);
+    /* Check if there is some error in conversion */
+    errno = 0;
+    long x = strtol(t->contents, NULL, 10);
+    return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
   }
 
   /* The operator is always the second child */
   char* op = t->children[1]->contents;
 
   /* We store the third child in 'x' */
-  long x = eval(t->children[2]);
+  lval x = eval(t->children[2]);
 
   /* Iterate over the remaining children and combining */
   int i = 3;
@@ -107,7 +165,7 @@ int main(int argc, char** argcv) {
   mpca_lang(MPCA_LANG_DEFAULT,
   "                                                     \
     number   : /-?[0-9]+/ ;                             \
-    operator : '+' | '-' | '*' | '/' | '%' | '^' ;                  \
+    operator : '+' | '-' | '*' | '/' | '%' | '^' ;      \
     expr     : <number> | '(' <operator> <expr>+ ')' ;  \
     lispy    : /^/ <operator> <expr>+ /$/ ;             \
   ",
@@ -136,8 +194,8 @@ int main(int argc, char** argcv) {
       printf("Number of branches: %i\n",  number_of_branches(a));
 
       /* On success print the AST */
-      long result = eval(r.output);
-      printf("Answer: %li\n", result);
+      lval result = eval(r.output);
+      lval_println(result);
       mpc_ast_delete(r.output);
     }else{
       /* Otherwise print the error */
